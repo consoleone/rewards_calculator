7eddfd8462b5a83b6f343df86188f2cdf59a5e5d
ABHIRAM REDDY <anukoolaabhiram@outlook.com>
2022-07-05 18:55:12 +0530

    update

--- src/server.js
+++ src/server.js
@@ -17,5 +17,5 @@ app.post("/api/get-rewards", (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("server is listening");
+  console.log("server started");
 });
