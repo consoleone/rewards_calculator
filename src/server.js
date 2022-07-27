886674e99e4e6d4e10f618dcb5d4acea90431392
ABHIRAM REDDY <anukoolaabhiram@outlook.com>
2022-07-27 14:01:42 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
-if (process.env.NODE_ENV === "development") require("dotenv").config();
-const express = require("express");
-const calculateRewards = require("./utils/getRewards");
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
+const express = require('express');
+const calculateRewards = require('./utils/getRewards');
+const cors = require('cors');
 
 const app = express();
 
+app.use(
+  cors({
+    origin: '*',
+    preflightContinue: true,
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
+
 app.use(express.json());
 
-app.post("/api/get-rewards", (req, res) => {
+app.post('/api/get-rewards', (req, res) => {
   const { address, startDate, endDate } = req.body;
   calculateRewards(address, startDate, endDate);
   res
     .status(200)
-    .json({ success: true, message: "Rewards will avaliable after 30min " });
+    .json({ success: true, message: 'Rewards will avaliable after 30min ' });
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("server started");
+  console.log('server started');
 });
