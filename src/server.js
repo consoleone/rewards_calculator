9ec95ffa80ebf9a30e291b9de82c0eef346250a7
ABHIRAM REDDY <anukoolaabhiram@outlook.com>
2022-08-13 20:05:10 +0530

    dates format fix

--- src/server.js
+++ src/server.js
@@ -15,7 +15,17 @@ app.use(express.json());
 
 app.post('/api/get-rewards', (req, res) => {
   const { address, startDate, endDate } = req.body;
-  calculateRewards(address, startDate, endDate);
+  let startDateNew = startDate.split('T')[0];
+  let endDateNew = endDate.split('T')[0];
+  if (
+    new Date(endDateNew).getTime() ===
+    new Date(new Date().toISOString().split('T')[0]).getTime()
+  ) {
+    const today = new Date();
+    today.setDate(today.getDate() - 1);
+    endDateNew = today.toISOString().split('T')[0];
+  }
+  calculateRewards(address, startDateNew, endDateNew);
   res
     .status(200)
     .json({ success: true, message: 'Rewards will avaliable after 30min ' });
