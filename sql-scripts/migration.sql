bd13c4bba8bf6a16d3adec30762c9142905a1ff1
Abhiram <anukoolaabhiram@outlook.com>
2024-03-16 22:36:58 +0530

    removed

--- sql-scripts/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "logs" (
-    "tag" Text NOT NULL,
-    "time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "data" JSONB NOT NULL,
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "Logs_pkey" PRIMARY KEY ("id")
-);
